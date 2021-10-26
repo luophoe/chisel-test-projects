d0c2c2ce7e19dd9e651e509fb8c8ee17d926c46c
Phoebe Luo <z65luo@uwaterloo.ca>
2021-10-26 16:39:41 +0800

    Create RisingMooreFsm.scala

---
+++ RisingMooreFsm/RisingMooreFsm.scala
@@ -0,0 +1,36 @@
+class RisingMooreFsm extends Module {
+  val io = IO(new Bundle {
+    val din = Input(Bool())
+    val risingEdge = Output(Bool())
+  })
+  
+    // The three states
+    val zero :: puls :: one :: Nil = Enum(3)
+    
+    // The state register
+    val stateReg = RegInit(zero)
+    
+    // Next state logic
+    switch(stateReg){
+        is(zero){
+            when(io.din){
+                stateReg := puls
+            }
+        }
+        is(puls){
+            when(io.din){
+                stateReg := one
+            }.otherwise{
+                stateReg := zero
+            }
+        }
+        is(one){
+            when(!io.din){
+                stateReg := zero
+            }
+        }
+    }
+    
+    // Output logic
+    io.risingEdge := stateReg === puls
+}
